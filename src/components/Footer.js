7bb16a24e60319018ecee787fca028496eef0581
Yasiru-Deshan <yasirudeshan3@gmail.com>
2023-12-13 23:50:37 +0530

    design updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,8 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <Container>
+      <Container style={{padding: "20px"}}>
         <Row className="align-items-center">
-          <MailchimpForm />
           <Col size={12} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
